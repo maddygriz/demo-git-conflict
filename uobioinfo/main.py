638b6ede903b6fd0d97218e5df4f1b03a42cfe9b
maddygriz <maddygriz@gmail.com>
2018-10-11 13:22:31 -0700

    nerds

--- uobioinfo/main.py
+++ uobioinfo/main.py
@@ -5,9 +5,7 @@ def say_something(phrase): # ... I'm giving up on you.
     """Change this however you'd like!"""
     print("testphrase1")
     print(phrase)
-    print("testphrase2")
-
-
+    print("Nerds!")
 # --- Don't change anything below this line -------
 
 
